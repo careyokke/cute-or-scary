db080f547956e5ec7ae881e3b4d73de7ee96942e
Emilie Thoreson and Yokke Hargono <emilie@>
2017-05-30 14:15:42 -0700

    complete a cute-or-scary project

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+  $("#clickable1").click(function() {
+    console.log("hello");
+    $("pic1").toggle();
+  });
+
+  $("#clickable2").click(function() {
+    console.log("hello");
+    $("#pic5").toggle();
+  });
+
+  $("#clickable3").click(function() {
+    console.log("hello");
+    $("#pic2").toggle();
+  });
+
+  $("#clickable4").click(function() {
+    console.log("hello");
+    $("#pic6").toggle();
+  });
+
+  $("#clickable5").click(function() {
+    console.log("hello");
+    $("#pic3").toggle();
+  });
+
+  $("#clickable6").click(function() {
+    console.log("hello");
+    $("#pic7").toggle();
+  });
+
+  $("#clickable7").click(function() {
+    console.log("hello");
+    $("#pic4").toggle();
+  });
+
+  $("#clickable8").click(function() {
+    console.log("hello");
+    $("#pic8").toggle();
+  });
+})
